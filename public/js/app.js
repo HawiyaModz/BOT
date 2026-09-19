868e1a367e1de02330204d1a3ec30b1f9a00fe48
HawiyaModz <hawiyamods@gmail.com>
2026-09-19 10:44:33 +0700

    Delete public/js/app.js

--- public/js/app.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){document.getElementById("nav").classList.toggle("show")}
-function toggleTheme(){
-  document.documentElement.classList.toggle("light");
-  localStorage.setItem("theme", document.documentElement.classList.contains("light") ? "light" : "dark");
-}
-if(localStorage.getItem("theme")==="light") document.documentElement.classList.add("light");
