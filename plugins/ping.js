d35929661465434d81139c85aec359e22ce8bf48
HawiyaModz <hawiyamods@gmail.com>
2026-09-19 04:18:05 +0000

    Delete plugins directory

--- plugins/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "ping",
-  aliases: ["p"],
-  description: "Cek apakah bot aktif.",
-  command: async ({ sock, jid }) => {
-    await sock.sendMessage(jid, { text: "🏓 Pong! JayPay Bot aktif." });
-  }
-};
